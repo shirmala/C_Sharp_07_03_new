9f25322ec6f75edee076255e6a1819d7f5c945e3
Timofeev <t9944m@yandex.ru>
2023-03-13 17:24:37 +0300

    Task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 1_3 Вывести на экран числа от -N до N
+//Супер-решение
+Console.WriteLine("Ввести  N");
+int N = int.Parse(Console.ReadLine()!);
+
+int count = -N;
+while (count!= N)
+{
+      Console.WriteLine($"{count}");
+      if (N>0) count +=1;
+      else count -=1;
+      }
+Console.WriteLine($"{count}");
+Console.WriteLine();
+
+//Мое решениею Некачественное
+/*
+Console.WriteLine("Ввести положительное число N");
+int N = int.Parse(Console.ReadLine()!);
+
+int count = -N;
+while (count <= N)
+{
+
+      Console.WriteLine(count);
+      {
+            count++;
+      }
+}
+Console.WriteLine();
+
+Console.WriteLine("Ввести отрицательное число K");
+int K = int.Parse(Console.ReadLine()!);
+
+int i = -K;
+while (i >= K)
+{
+      Console.WriteLine(i);
+      {
+            i--;
+      }
+}
+Console.WriteLine();
+*/
