e8566a32b292fdd10c3d17c5eedfa99a5220203a
Timofeev <t9944m@yandex.ru>
2023-03-13 16:48:48 +0300

    Task 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 1_0. Напишите программу, которая на вход принимает число
+// и выдает его квадрат
+
+Console.WriteLine ( "Введите число");
+int num = int.Parse (Console.ReadLine()!);
+Console.WriteLine (num*num);
+
+
