ff7b14a893610605bb6ab9a31c8f05bffdbb112c
Timofeev <t9944m@yandex.ru>
2023-03-14 08:59:09 +0300

    Task 2_0

---
+++ Lesson_2/2_0/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 2_0. Функция
+
+int TakeNum (int num)
+{
+Console.WriteLine (num);
+return num%10;
+}
+
+int th_num = new Random().Next(100,1000);
+              // это как формула генератора 
+              // случ чисел
+TakeNum(th_num);// th_num отличается от num!!
+
+int result = TakeNum(th_num);
+Console.WriteLine (result);
+
