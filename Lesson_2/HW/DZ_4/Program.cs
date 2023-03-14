2e6ef26fd98451f6b12d1c000e30f9c2976a6c36
Timofeev <t9944m@yandex.ru>
2023-03-14 19:26:03 +0300

    DZ_4 Lesson_2

---
+++ Lesson_2/HW/DZ_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача DZ_4.
+//Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+//Вариант от преподавателя. Красивое решение:
+void Weekend(int num)
+{
+      string text = "not quit a week";// сваеобразный флаг!
+      if (num == 7 || num == 6) text = "Weekend";
+      else if (num < 6 && num > 0) text = "Workday";
+      Console.WriteLine($"{num}         {text}");
+}
+int val = int.Parse(Console.ReadLine()!);
+Weekend(val);
