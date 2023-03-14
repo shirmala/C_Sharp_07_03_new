a61dd7c144bed6a73e316fa747b08ba898ff7247
Timofeev <t9944m@yandex.ru>
2023-03-14 18:54:36 +0300

    DZ_3 Lesson_2

---
+++ Lesson_2/HW/DZ_3/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача DZ_3.
+//Напишите программу, которая выводит третью цифру заданного
+// числа или сообщает, что третьей цифры нет.
+
+//Вариант от преподавателя:
+
+void ThirdNum(int num)
+{
+      Console.Write($"{num}    ");
+      if (num < 100)
+      {
+            Console.WriteLine("There is no third-digit");
+            return;
+      }
+      while (num > 999) num /= 10;
+      Console.WriteLine(num % 10);
+}
+int val = int.Parse(Console.ReadLine()!);
+ThirdNum(val);
