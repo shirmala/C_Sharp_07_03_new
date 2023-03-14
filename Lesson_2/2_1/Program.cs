f8dd7b5a00995c47c7c2fcf143cc0d7864427065
Timofeev <t9944m@yandex.ru>
2023-03-14 09:02:36 +0300

    Task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 2_1. Напишите программу, которая выводит случайное
+// число из отрезка [10,99] и показывает наибольшую цифру числа
+
+int BigNum(int num)
+{
+      int f = num / 10;
+      int s = num % 10;
+      if (f > s) Console.WriteLine(f);
+      if (f < s) Console.WriteLine(s);
+      if (f == s) Console.WriteLine(" Цифры одинаковые");
+      return (num);
+}
+int b_num = new Random().Next(10, 100);
+int result = BigNum(b_num);
+Console.WriteLine(result);
