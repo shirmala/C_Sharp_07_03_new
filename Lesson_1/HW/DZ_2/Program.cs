25235b2165a92d173b53f8ad50f65aa2300da85c
Timofeev <t9944m@yandex.ru>
2023-03-14 07:36:49 +0300

    DZ_2 Lesson_1

---
+++ Lesson_1/HW/DZ_2/Program.cs
@@ -0,0 +1,40 @@
+﻿// DZ_2. Напишите программу, которая принимает на вход три числа и
+// выдаёт максимальное из этих чисел.
+/*
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+/*
+Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+
+if (a > b)
+{
+      if (a > c) Console.WriteLine($"Максимальное {a}");
+      if (a < c) Console.WriteLine($"Максимальное {c}");
+}
+if (a < b)
+{
+      if (b > c) Console.WriteLine($"Максимальное {b}");
+      if (b < c) Console.WriteLine($"Максимальное {c}");
+}
+*/
+//Решение от преподавателя
+//Очень понравилось это решение. Гениальное!
+
+Console.WriteLine("Введите три числа");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+if (a<b)
+{
+a=b;
+}
+if (a<c)
+{
+a=c;
+}
+Console.WriteLine ($"число {a} самое большое");
