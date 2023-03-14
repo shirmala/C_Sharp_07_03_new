489347667d70bc58be04283343277fe72023cd55
Timofeev <t9944m@yandex.ru>
2023-03-14 16:05:49 +0300

    DZ_1 Lesson_2

---
+++ Lesson_2/HW/DZ_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача DZ_1 
+//Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает вторую цифру этого числа.
+
+//Вариант от преподавателя:
+
+string NumSec (int num)
+{
+if ((-1000< num && num>-100) || (num <1000 && num >100))
+    return $"{num/10 % 10}";
+    return "The numder is not three-dirit !";
+}
+Console.WriteLine (NumSec (  int.Parse (Console.ReadLine())));   
+
+/*
+
+//ВАриант мой:
+
+int n=int.Parse(Console.ReadLine());
+if (n<100)Console.WriteLine("NO");
+else Console.WriteLine(n/10%10);
+*/
---
+++ Lesson_3/3_0/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 3_0  Напишите программу, которая принимает на вход
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string  NumSec (int num)
+{
+if( (-1000<num && num < -100) || (num < 1000 && num > 100) )
+return $"{num /10 % 10}";
+return "The nuber is not three-digit!";
+
+}
+Console.WriteLine (NumSec (int.Parse(Console.ReadLine())));
+
