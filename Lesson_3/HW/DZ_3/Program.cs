08594224d8edbba8021cc8e0a83f3e77512c0590
Timofeev <t9944m@yandex.ru>
2023-03-17 19:21:50 +0300

    DZ_3 Lesson_3

--- Lesson_3/HW/DZ_3/Program.cs
+++ Lesson_3/HW/DZ_3/Program.cs
@@ -3,14 +3,30 @@
 //выдает таблтцу кубов чисел от 1 до N
 
 //  Вариант 1.
-int N = int.Parse(Console.ReadLine()!);
-Console.WriteLine();
-int i = 1;
-for (i = 1; i <= N; i++)
+void KubNum (int a)
+{
+ int i = 1;
+for (i = 1; i <= a; i++)
 {
   Console.WriteLine($"{i * i * i}");
 }
+}
+int b = int.Parse(Console.ReadLine()!);
+KubNum(b);
 
 //Вариант 2. Решение от преподавателя
 
-
+void Squares (int n)
+{
+double[]  nums = new double [n];
+int index = 0;
+Console.Write($"{n}    ");
+while(index < n)
+{
+nums[index] = Math.Pow((index+1),3);
+Console.Write($"{nums[index]},   ");
+index+=1;
+}
+Console.WriteLine();
+}
+Squares(int.Parse(Console.ReadLine()!));// Здорово!!Очень понравилось
