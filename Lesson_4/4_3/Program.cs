333cdda728d8c61c6faa02caacf325fc2ec5946b
Timofeev <t9944m@yandex.ru>
2023-03-18 20:22:34 +0300

    Task 4_3

---
+++ Lesson_4/4_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 4_3.Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+PrintArray(MakeArray());
+int[] MakeArray()
+{
+      int[] arr = new int[8];
+      for (int i = 0; i < 8; i++)
+      {
+            arr[i] = GenerateNumber();
+      }
+      return arr;
+}
+int GenerateNumber()
+{
+      int number = new Random().Next(2);
+      return number;
+}
+void PrintArray(int[] array)
+{
+      for (int i = 0; i < array.Length; i++)
+      {
+            Console.Write($"{array[i]}   ");
+      }
+      Console.WriteLine();
+}
