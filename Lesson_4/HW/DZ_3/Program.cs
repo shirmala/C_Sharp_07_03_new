fbf30ccef4262b062df37739bc5197b26b886a49
Timofeev <t9944m@yandex.ru>
2023-03-22 07:57:37 +0300

    DZ_3 Lesson_4

--- Lesson_4/HW/DZ_3/Program.cs
+++ Lesson_4/HW/DZ_3/Program.cs
@@ -4,6 +4,28 @@
 //Оформите оформление массива и вывода в виде функции (пригодится
 //в следующих задачах).
 
+//Вариант от преподавателя:
+void Print(int[] arr)
+{
+int size = arr.Length;
+for (int i=0;i<size;i++)
+Console.Write($"{arr[i]}   ");
+Console.WriteLine();
+}
+int[] EightMass(int size,int from, int to)
+{
+int[] arr = new int[size];
+for (int i=0;i<size;i++)
+arr[i] = new Random().Next(from,to +1);
+return arr;
+}
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = EightMass(num,start,stop);
+Print (mass);
+
 //Вариант_1: 
 void FillArray(int[] a)
 {
