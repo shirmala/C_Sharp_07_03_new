ae75369f047d337aa9c2082463566465ed3fc4e8
Timofeev <t9944m@yandex.ru>
2023-03-23 10:21:20 +0300

    text

---
+++ Lesson_5/HW/DZ_1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+//Задача DZ_1
+
+//Задайте массив заполненный случайными положительными 
+//трёхзначными числами. Напишите программу, которая покажет
+// количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+void Print(int[] arr)
+{
+      int size = arr.Length;
+      for (int i = 0; i < size; i++)
+            Console.Write($"{arr[i]}   ");
+      Console.WriteLine();
+}
+
+int[] ThreeDigitMass(int size, int from, int to)
+{
+      int[] arr = new int[size];
+
+      for (int i = 0; i < size; i++)
+            arr[i] = new Random().Next(from, to + 1);
+      return arr;
+}
+void CountEvenNumber(int[] arr)
+{
+      int count = 0;
+      for (int i = 0; i < arr.Length; i++)
+      {
+            if (arr[i] % 2 == 0)
+                  count++;
+      }
+      Console.WriteLine($"{count} четных чисел");
+}
+int num = 8; //int.Parse(Console.ReadLine()!);
+int start = 100; //int.Parse(Console.ReadLine()!);
+int stop = 1000;//int.Parse(Console.ReadLine()!);
+
+int[] mass = ThreeDigitMass(num, start, stop);
+Print(mass);
+CountEvenNumber(mass);
---
+++ Lesson_5/HW/DZ_3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача DZ_3.
+//Задайте массив вещественных чисел. Найдите разницу между
+// максимальным и минимальным элементов массива.
+//[3 7 22 2 78] -> 76
+
+void Print(int[] arr)
+{
+      int size = arr.Length;
+      for (int i = 0; i < arr.Length; i++)
+            System.Console.Write($"{arr[i]}    ");
+}
+int[] MaxMinIntArray(int size, int from, int to)
+{
+      int[] arr = new int[size];
+      for (int i = 0; i < size; i++)
+            arr[i] = new Random().Next(from, to);
+      return arr;
+}
+int Max(int[] a)
+{
+      int m = a[0];
+      for (int i = 1; i < a.Length; i++)
+            if (a[i] > m) m = a[i];
+      return m;
+}
+int Min(int[] a)
+{
+      int m = a[0];
+      for (int i = 1; i < a.Length; i++)
+            if (a[i] < m) m = a[i];
+      return m;
+}
+
+int[] a = MaxMinIntArray(10, 0, 10);// выбираю 9 чисел
+                                    // от 0 до 10
+Print(a);
+Console.WriteLine();
+Console.WriteLine($"Разница: Max(a)-Min(a) равна");
+Console.WriteLine(Max(a) - Min(a));
+
