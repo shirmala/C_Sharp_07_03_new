68bde16d3a92c4800ec958a20ab58b3f1d6d7e9e
Timofeev <t9944m@yandex.ru>
2023-03-28 15:14:31 +0300

    Task 6_2

---
+++ Lesson_6/6_2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 6_2
+// Напишите программу, которая будет
+// преобразовывать десятичное число в двоичное.
+
+int GetUserNumber(string name)
+{
+      int number = 0;
+      Console.Write($"Please enter {name}: ");
+      number = int.Parse(Console.ReadLine()!);
+      return number;
+}
+string MakeBinary(int number)
+{
+      string binary = "";
+      while (number > 0)
+      {
+            binary = number % 2 + binary;
+            number /= 2;
+      }
+      return binary;
+}
+int num = GetUserNumber("number");
+Console.WriteLine(MakeBinary(num));
+
