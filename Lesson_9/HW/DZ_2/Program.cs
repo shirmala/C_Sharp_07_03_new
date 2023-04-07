88072943c2c4ec214813a8365dd679b9f3c3fa3b
Timofeev <t9944m@yandex.ru>
2023-04-07 22:35:06 +0300

    DZ_2 Lesson_9

---
+++ Lesson_9/HW/DZ_2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача DZ_2.
+/* Задайте значения M и N. Напишите программу, которая найдёт сумму
+натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8 -> 30
+*/
+// Вариант с рекурсией
+int Sum(int m, int n)
+{
+      if (n == m) return m;
+      else return n + Sum(m, n - 1);
+}
+Console.WriteLine(Sum(10, 13));
+
+//Вариант без рекурсии:
+int SumA(int m, int n)
+{
+      int rez = 0;
+      for (int i = m; i <= n; i++) rez += i;
+      return rez;
+}
+Console.WriteLine(SumA(2, 4));
+
