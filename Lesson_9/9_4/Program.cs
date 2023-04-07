5cb863778d9b6c9562304d3b5cc10df2c1b612d5
Timofeev <t9944m@yandex.ru>
2023-04-07 19:11:07 +0300

    Task_9_4 Lesson_9

---
+++ Lesson_9/9_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 9_4.
+//Напишите программу, которая на вход принимает два числа A и B,
+//    и возводит число А в целую степень B с помощью рекурсии.
+
+double StepNum (int a,int b)
+{
+if(b>0) return StepNum (a,b-1)*a;
+if(b<0) return   StepNum (a,b+1)*1/a; // StepNum (a,b+1)/a;
+else return 1;  
+}
+Console.WriteLine(StepNum (3,2));
+Console.WriteLine(StepNum (3,-2));
+Console.WriteLine(StepNum (3,0));
+
