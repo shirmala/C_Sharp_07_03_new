d4e950dc96aaec3eb0d3cf2b4bd4aa312fd9cab9
Timofeev <t9944m@yandex.ru>
2023-04-07 19:02:29 +0300

    Task_9_1 Lesson_9

---
+++ Lesson_9/9_1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 9_1.
+/*
+Задайте значения M и N. Напишите программу, которая
+рекурсивно выведет все натуральные числа в  промежутке
+от M до N.
+M = 1, N = 5  > 1,2,3,4,5
+M = 4, N = 8  > 4,6,7,8
+*/
+int M = GetUserNumber("  Введите M");
+int N = GetUserNumber("  Введите N");
+PrintN(M, N);
+Console.WriteLine();
+
+int GetUserNumber(string name)
+{
+      int number = 0;
+      Console.Write($" Please enter {name}:");
+      number = int.Parse(Console.ReadLine()!);
+      return number;
+}
+void PrintN(int begin, int end)
+{
+      if (begin - 1 == end)
+      {
+            return;
+      }
+
+      PrintN(begin, end - 1);
+      Console.Write($"{end}   ");
+}
